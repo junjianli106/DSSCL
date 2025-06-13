416c4d6e43bc76a4eaba101018aee77bd0a4c5e3
junjianli <38580237+junjianli106@users.noreply.github.com>
2025-06-13 22:33:36 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-python main_w_he.py --weight_orig 0.3 --num-cluster '40' --resume '/homec/xxxxx/python_projects/Pathological_Image_Classification/SSCL/experiment_w_he/checkpoint_0020.pth.tar'
-python main_w_he.py --weight_orig 0.0 --num-cluster '10'
-python main_w_he.py --weight_orig 0.1 --num-cluster '10'
-python main_w_he.py --weight_orig 0.2 --num-cluster '10'
-python main_w_he.py --weight_orig 0.3 --num-cluster '10'
+python main_w_he.py --weight_orig 0.3 --num-cluster '40'
